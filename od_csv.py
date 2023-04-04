f5161f2f3743ea135ddcf736e9bd1d934606e6aa
EduFalbel <du.falbel@gmail.com>
2023-04-04 17:17:29 +0200

    Second version

--- od_csv.py
+++ od_csv.py
@@ -4,53 +4,33 @@ import pandas as pd
 import geopandas as gpd
 import shapely.geometry as shp
 
-import matsim
-
-
-def turn_nodes_into_gdf(nodes: pd.DataFrame, crs: str) -> gpd.GeoDataFrame:
-    return gpd.GeoDataFrame(nodes.assign(geometry = shp.Point(nodes['x'], nodes['y']))).set_crs(crs).drop(columns=['x', 'y'])
-
-def associate_nodes_with_zones(zones: gpd.GeoDataFrame, nodes: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """Finds out which nodes are contained within each zones"""
-    return nodes.sjoin(zones, how='left', predicate='within')
-
-def associate_links_with_zones(zones: gpd.GeoDataFrame, links: pd.DataFrame, nodes: pd.DataFrame, crs: str = 'LV95') -> pd.DataFrame:
-    """
-    Finds out the start and end zones for each link based on the start and end nodes
-    """
-    if isinstance(nodes, gpd.GeoDataFrame):
-        nodes = turn_nodes_into_gdf(nodes, crs)
-    nodes_with_zones = associate_nodes_with_zones(zones, nodes)
-    
-    return links\
-        .merge(nodes_with_zones, left_on='from_node', right_on='node_id')\
-        .merge(nodes_with_zones, left_on='to_node', right_on='node_id', suffixes=['_from', '_to'])\
-
-def get_link_counts(events) -> pd.DataFrame:
-    """Get counts for each link"""
-    
-    link_counts = defaultdict(int)
-
-    for event in events:
-        link_counts[event['link']] += 1
-
-    link_counts = pd.DataFrame.from_dict(link_counts, orient='index', columns=['count']).rename_axis('link_id')
-
-def aggregate_zones_with_volumes(links_with_zones: pd.DataFrame, link_counts: pd.DataFrame, zone_id_col: str = 'zone_id') -> pd.DataFrame:
-    """Given a dataframe of links with respective vehicle counts and associated start and end zones, returns a dataframe"""
-    return links_with_zones.merge(link_counts, on='link_id').groupby(by=[f"{zone_id_col}_from", f"{zone_id_col}_to"])[['count']].sum().reset_index().rename({f"{zone_id_col}_from" : "FROM", f"{zone_id_col}_to": "TO", "count": "DEMAND"})
 
 if __name__ == '__main__':
     from sys import argv
 
-    zone_path, network_path, events_path = argv[1:]
+    trips_file, zones_file, zone_id_col = argv[1:]
 
-    zones: gpd.GeoDataFrame = gpd.read_file(zone_path)
+    trips = pd.read_csv(trips_file)
+    trips: pd.DataFrame = trips[trips['modes'].str.contains('pt')][['start_x', 'start_y', 'end_x', 'end_y']]
+    trips['geometry_start'] = trips.apply(lambda x: shp.Point(x['start_x'], x['start_y']), axis=1)
+    trips['geometry_end'] = trips.apply(lambda x: shp.Point(x['end_x'], x['end_y']), axis=1)
+    trips.drop(columns=['start_x', 'start_y', 'end_x', 'end_y'], inplace=True)
 
-    network = matsim.read_network(network_path)
-    nodes, links = network.nodes, network.links
+    print('Assigned geometry')
 
-    events = matsim.event_reader(events_path, types='entered link')
+    zones: gpd.GeoDataFrame = gpd.read_file(zones_file)[["ID", "geometry"]]
+    print(zones.head())
 
-    aggregate_zones_with_volumes(associate_links_with_zones(zones, links, nodes), get_link_counts(events)).to_csv("od_matrix.csv")
+    trips = gpd.GeoDataFrame(trips).set_geometry('geometry_start')
+    trips = trips.sjoin(zones, how='left', predicate='within')
+    print(trips.head())
+
+    trips = trips.set_geometry('geometry_end')
+    trips = trips.sjoin(zones, how='left', predicate='within', lsuffix='start', rsuffix='end')
+    print(trips.head())
+
+    print('Spatial join')
+
+    zones_with_volumes = trips.groupby([f"{zone_id_col}_start", f"{zone_id_col}_end"]).count().iloc[:, 0].reset_index().rename({f"{zone_id_col}_start" : "FROM", f"{zone_id_col}_end": "TO", trips.columns[0]: "DEMAND"})
+    zones_with_volumes.to_csv('od_matrix.csv', index=False)
 
